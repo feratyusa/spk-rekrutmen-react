92aadabbd3cb05fdc71a898fcb311577d9c663e8
feratyusa <prabuega55@gmail.com>
2023-10-19 15:31:26 +0700

    test: mock data

---
+++ src/global/DataExample.js
@@ -0,0 +1,21 @@
+const DataExample =
+[
+    {
+        "id": 1,
+        "name": "Mini Dummy",
+        "description": "Example Data",
+        "created_at": "2023-04-01",
+        "updated_at": "2023-04-05",
+        "file_path": "/uploads/user/data.csv"
+    },
+    {
+        "id": 2,
+        "name": "Data Perusahaan Kecantikan",
+        "description": "400 Calon Karyawan",
+        "created_at": "2023-04-10",
+        "updated_at": "2023-07-01",
+        "file_path": "/uploads/user/data2.csv"
+    }
+]
+
+export default DataExample;
